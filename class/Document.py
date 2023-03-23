6ef7a883a8096f05ece1a3c11287fa5a6869a6be
vdh1612 <125654739+vdh1612@users.noreply.github.com>
2023-03-23 18:47:17 +0700

    Document_class

--- class/Document.py
+++ class/Document.py
@@ -1,3 +1,4 @@
+from function import addDocument,delDocument,modDocument,showDocument
 class Document:
     def __init__(self,name,author,publisher,yearPublish,note):
         self.name = name 
@@ -5,24 +6,24 @@ class Document:
         self.publisher = publisher 
         self.yearPublish = yearPublish
         self.note = note
-    def set_name(self):
-        pass
+    def set_name(self,name):
+        self.name = name
     def get_name(self):
         return self.name 
-    def set_author(self):
-        pass
+    def set_author(self,author):
+        self.author = author
     def get_author(self):
         return self.author
-    def set_publisher(self):
-        pass
+    def set_publisher(self,publisher):
+        self.publisher = publisher
     def get_publisher(self):
         return self.publisher
-    def set_yearPublisher(self):
-        pass
+    def set_yearPublisher(self,yearPublisher):
+        self.yearPublisher = yearPublisher 
     def get_yearPublish(self):
         return self.yearPublish
-    def set_note(self):
-        pass
+    def set_note(self,note):
+        self.note = note
     def get_note(self) :
         return self.note
     
