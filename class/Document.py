8bd8ce729fd7080c4780ba782c45c14ea93c9419
vdh1612 <125654739+vdh1612@users.noreply.github.com>
2023-03-23 16:21:03 +0700

    Document_class

---
+++ class/Document.py
@@ -0,0 +1,28 @@
+class Document:
+    def __init__(self,name,author,publisher,yearPublish,note):
+        self.name = name 
+        self.author = author 
+        self.publisher = publisher 
+        self.yearPublish = yearPublish
+        self.note = note
+    def set_name(self):
+        pass
+    def get_name(self):
+        return self.name 
+    def set_author(self):
+        pass
+    def get_author(self):
+        return self.author
+    def set_publisher(self):
+        pass
+    def get_publisher(self):
+        return self.publisher
+    def set_yearPublisher(self):
+        pass
+    def get_yearPublish(self):
+        return self.yearPublish
+    def set_note(self):
+        pass
+    def get_note(self) :
+        return self.note
+    
