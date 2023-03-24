0c7761452981946ed63c07b1091cd37be6119246
wynnn3301 <124663026+wynnn3301@users.noreply.github.com>
2023-03-24 23:13:14 +0700

    Update delDocument.py

--- function/delDocument.py
+++ function/delDocument.py
@@ -1,1 +1,8 @@
 # Must return a Document
+def delDocument(self, name) -> bool:
+        # Find the document with the specified name and remove it from the list
+        for document in self.__document_list:
+            if document.get_name() == name:
+                self.__document_list.remove(document)
+                return True
+        return False
