60f8b47540d95013966bf3a990a957ed2123ee1b
Basteur1810 <102292325+datatype123@users.noreply.github.com>
2023-03-24 12:15:23 +0700

    Update showDocument.py

--- function/showDocument.py
+++ function/showDocument.py
@@ -1,1 +1,16 @@
 # Must return a Document
+# Must return a Document
+def show_document():
+    #my_doc is example to check code run or not
+    my_doc = {
+    'name': 'The Catcher in the Rye',
+    'author': 'J.D. Salinger',
+    'publisher': 'Little, Brown and Company',
+    'yearPublish': 1951,
+    'note': 'A classic coming-of-age novel'
+    }
+
+    modified_doc = modify_document(my_doc)
+    print(modified_doc)
+
+#coombine the mod function, show function and class Document to check this code is run or not
