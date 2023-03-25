40ad8bf0af76c59b3552d133ece27de9c6a6ebbc
wynnn3301 <124663026+wynnn3301@users.noreply.github.com>
2023-03-25 15:33:03 +0700

    Update addDocument.py

--- function/addDocument.py
+++ function/addDocument.py
@@ -1,12 +1,14 @@
 # Must return a Document
 def addDocument(document_list):
-    # Get the details of the new document from the user
-    title = input("Enter the title of the document: ")
-    author = input("Enter the author of the document: ")
-    publisher = input("Enter the publisher of the document: ")
-    year = input("Enter the year of publication: ")
-    note = input("Enter any notes about the document: ")
+    _title = input("Enter the title of the document: ")
+    _author = input("Enter the author of the document: ")
+    _publisher = input("Enter the publisher of the document: ")
+    _year = input("Enter the year of publication: ")
+    _note = input("Enter any notes about the document: ")
 
     # Create a new Document object and add it to the list
-    new_document = Document(title, author, publisher, year, note)
+    new_document = Document(_title, _author, _publisher, _year, _note)
     document_list.append(new_document)
+
+    # Return the new document object
+    return new_document
