bfd1d0b71c4272c798faf83de34415cbcaaf90f2
wynnn3301 <124663026+wynnn3301@users.noreply.github.com>
2023-03-26 20:41:40 +0700

    Update addDocument.py

--- function/addDocument.py
+++ function/addDocument.py
@@ -1,14 +1,17 @@
 # Must return a Document
-def addDocument(document_list):
-    _title = input("Enter the title of the document: ")
-    _author = input("Enter the author of the document: ")
-    _publisher = input("Enter the publisher of the document: ")
-    _year = input("Enter the year of publication: ")
-    _note = input("Enter any notes about the document: ")
+from document import Document
+
+def addDocument(dm):
+    # Prompt the user for metadata
+    __title = input("Enter the title of the document: ")
+    __author = input("Enter the author of the document: ")
+    __publisher = input("Enter the publisher of the document: ")
+    __year = input("Enter the year of publication: ")
+    __note = input("Enter any notes about the document: ")
 
     # Create a new Document object and add it to the list
-    new_document = Document(_title, _author, _publisher, _year, _note)
-    document_list.append(new_document)
+    new_document = Document(__title, __author, __publisher, __year, __note)
+    dm._add_document(new_document)
 
     # Return the new document object
     return new_document
