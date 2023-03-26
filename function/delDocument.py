9f4f0ddd9906f084aa3bf44221b6f9526d20f3d7
wynnn3301 <124663026+wynnn3301@users.noreply.github.com>
2023-03-26 21:19:18 +0700

    Update delDocument.py

--- function/delDocument.py
+++ function/delDocument.py
@@ -1,14 +1,17 @@
 # Must return a Document
-def delDocument(document_list, name):
+def delDocument(dm, name):
     # Find the document with the given name
-    for document in document_list:
-        if document.get_name() == name:
+    for document in dm._get_document_list():
+        if document._get_name() == name:
             # Remove all properties of the document
-            document.set_name(None)
-            document.set_author(None)
-            document.set_publisher(None)
-            document.set_yearPublish(None)
-            document.set_note(None)
+            document._set_name(None)
+            document._set_author(None)
+            document._set_publisher(None)
+            document._set_yearPublisher(None)
+            document._set_note(None)
+            
+            # Remove the document from the list
+            dm._get_document_list().remove(document)
 
             # Return the modified document object
             return document
